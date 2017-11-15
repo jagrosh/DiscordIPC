41f55a747158ad2a14d55ebe65b014f99d3fc61f
TheMonitorLizard <kaidangustave@yahoo.com>
2017-11-15 15:00:57 -0600

    Documented User

--- src/main/java/com/jagrosh/discordipc/entities/User.java
+++ src/main/java/com/jagrosh/discordipc/entities/User.java
@@ -16,6 +16,10 @@
 package com.jagrosh.discordipc.entities;
 
 /**
+ * A encapsulation of a Discord User's data provided when a
+ * {@link com.jagrosh.discordipc.IPCListener IPCListener} fires
+ * {@link com.jagrosh.discordipc.IPCListener#onActivityJoinRequest(com.jagrosh.discordipc.IPCClient, String, User)
+ * onActivityJoinRequest}.
  *
  * @author John Grosh (john.a.grosh@gmail.com)
  */
@@ -25,7 +29,11 @@ public class User
     private final String discriminator;
     private final long id;
     private final String avatar;
-    
+
+    /**
+     * Constructs a new {@link User}.<br>
+     * Only implemented internally.
+     */
     public User(String name, String discriminator, long id, String avatar)
     {
         this.name = name;
@@ -33,58 +41,121 @@ public class User
         this.id = id;
         this.avatar = avatar;
     }
-    
+
+    /**
+     * Gets the Users account name.
+     *
+     * @return The Users account name.
+     */
     public String getName()
     {
         return name;
     }
-    
+
+    /**
+     * Gets the Users discriminator.
+     *
+     * @return The Users discriminator.
+     */
     public String getDiscriminator()
     {
         return discriminator;
     }
-    
+
+    /**
+     * Gets the Users Snowflake ID as a {@code long}.
+     *
+     * @return The Users Snowflake ID as a {@code long}.
+     */
     public long getIdLong()
     {
         return id;
     }
-    
+
+    /**
+     * Gets the Users Snowflake ID as a {@code String}.
+     *
+     * @return The Users Snowflake ID as a {@code String}.
+     */
     public String getId()
     {
         return Long.toString(id);
     }
-    
+
+    /**
+     * Gets the Users avatar ID.
+     *
+     * @return The Users avatar ID.
+     */
     public String getAvatarId()
     {
         return avatar;
     }
-    
+
+    /**
+     * Gets the Users avatar URL.
+     *
+     * @return The Users avatar URL.
+     */
     public String getAvatarUrl()
     {
         return getAvatarId() == null ? null : "https://cdn.discordapp.com/avatars/" + getId() + "/" + getAvatarId()
             + (getAvatarId().startsWith("a_") ? ".gif" : ".png");
     }
-    
+
+    /**
+     * Gets the Users {@link DefaultAvatar} avatar ID.
+     *
+     * @return The Users {@link DefaultAvatar} avatar ID.
+     */
     public String getDefaultAvatarId()
     {
         return DefaultAvatar.values()[Integer.parseInt(getDiscriminator()) % DefaultAvatar.values().length].toString();
     }
-    
+
+    /**
+     * Gets the Users {@link DefaultAvatar} avatar URL.
+     *
+     * @return The Users {@link DefaultAvatar} avatar URL.
+     */
     public String getDefaultAvatarUrl()
     {
         return "https://discordapp.com/assets/" + getDefaultAvatarId() + ".png";
     }
-    
+
+    /**
+     * Gets the Users avatar URL, or their {@link DefaultAvatar} avatar URL if they
+     * do not have a custom avatar set on their account.
+     *
+     * @return The Users effective avatar URL.
+     */
     public String getEffectiveAvatarUrl()
     {
         return getAvatarUrl() == null ? getDefaultAvatarUrl() : getAvatarUrl();
     }
-    
+
+    /**
+     * Gets whether or not this User is a bot.<p>
+     *
+     * While, at the time of writing this documentation, bots cannot
+     * use Rich Presence features, there may be a time in the future
+     * where they have such an ability. <s>And we copy pasted this
+     * from JDA anyways, so why care.</s> :^)
+     *
+     * @return False
+     */
     public boolean isBot()
     {
         return false; //bots cannot use RPC
     }
-    
+
+    /**
+     * Gets the User as a discord formatted mention.<p>
+     *
+     * {@code <@SNOWFLAKE_ID> }
+     *
+     * @return A discord formatted mention of this User.
+     */
     public String getAsMention()
     {
         return "<@" + id + '>';
@@ -110,7 +181,11 @@ public class User
     {
         return "U:" + getName() + '(' + id + ')';
     }
-    
+
+    /**
+     * Constants representing one of five different
+     * default avatars a {@link User} can have.
+     */
     public enum DefaultAvatar
     {
         BLURPLE("6debd47ed13483642cf09e832ed0bc1b"),
